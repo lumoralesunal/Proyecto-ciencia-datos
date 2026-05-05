7749b145ac98c081747ec1900f546965e82a96a1
lumoralesunal <147669766+lumoralesunal@users.noreply.github.com>
2026-05-05 17:49:34 -0500

    actualizacion de variables app.js

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 function simular_prediccion() {
-
     var edad = document.getElementById("input_edad").value;
     var calificacion = document.getElementById("input_calificacion").value;
-    var clima = document.getElementById("input_clima").value;
-    var trafico = document.getElementById("input_trafico").value;
+    var rest_lat = document.getElementById("rest_lat").value;
+    var rest_lon = document.getElementById("rest_lon").value;
+    var del_lat = document.getElementById("del_lat").value;
+    var del_lon = document.getElementById("del_lon").value;
+    var orden = document.getElementById("input_orden").value;
     var vehiculo = document.getElementById("input_vehiculo").value;
-
 
     if (edad === "" || calificacion === "") {
         alert("Por favor, llena la edad y la calificación.");
         return; 
     }
 
-    fetch("http://127.0.0.1:5000/predict", {
+    fetch("http://localhost:5000/predict", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -20,6 +21,10 @@ function simular_prediccion() {
         body: JSON.stringify({
             edad: parseFloat(edad),
             calificacion: parseFloat(calificacion),
+            rest_lat: parseFloat(rest_lat),
+            rest_lon: parseFloat(rest_lon),
+            del_lat: parseFloat(del_lat),
+            del_lon: parseFloat(del_lon),
             orden: orden,
             vehiculo: vehiculo
         })
@@ -28,11 +33,15 @@ function simular_prediccion() {
         return response.json();
     })
     .then(function(data) {
-        
         var caja = document.getElementById("caja_resultado");
         var texto = document.getElementById("texto_resultado");
-      
-    caja.style.display = "block";
-    texto.innerHTML = "Tiempo estimado: x minutos.";
-})
+        
+        caja.style.display = "block";
+        texto.innerHTML = "Distancia: " + data.distancia_km + " km <br> Tiempo estimado: " + data.tiempo_predicho + " minutos.";
+    })
+    .catch(function(error) {
+        alert("El error real es: " + error.message);
+        console.error("Detalle del error:", error);
+    });
 }
+
